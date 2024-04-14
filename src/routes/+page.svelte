043f046639cf1fccc3216be53338bfa6f6693f62
Synaptic Synergy <synaptic.synergy.yt@gmail.com>
2024-04-14 12:22:33 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Synaptic Synergy</h1>
+<ul>
+    <li><a href="https://www.youtube.com/@Synaptic-Synergy">Youtube</a></li>
+    <li><a href="scheduler">Youtube scheduler tool</a></li>
+    <li><a href="sudoku">Sudoku tool</a></li>
+</ul>
