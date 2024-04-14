c26a2c9fc670d7fae313491ad41b10e431e7a01e
Synaptic Synergy <synaptic.synergy.yt@gmail.com>
2024-04-14 13:11:00 +0200

    add scheduler route

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export const buildURL = (base: string, obj: Record<string,string>) =>
+    `${base}?${Object.entries(obj)
+          .map(pair => pair.map(encodeURIComponent).join('='))
+          .join('&')}`;
---
+++ src/routes/scheduler/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { buildURL } from '$lib';
+
+	function login() {
+		const queryParams = {
+			client_id: '163691415601-9dkcmmbel076laqtobf51j29u4nduq3a.apps.googleusercontent.com',
+			redirect_uri: window.location.href,
+			response_type: 'token',
+			scope: 'https://www.googleapis.com/auth/youtube'
+		};
+		window.location.href = buildURL(`https://accounts.google.com/o/oauth2/v2/auth`, queryParams);
+	}
+</script>
+
+<button onclick={login}>Login</button>
