ce13865cb4048baeccf0712100e60c50114273c7
Synaptic Synergy <synaptic.synergy.yt@gmail.com>
2024-04-14 12:46:30 +0200

    add font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
-<h1>Synaptic Synergy</h1>
-<ul>
-    <li><a href="https://www.youtube.com/@Synaptic-Synergy">Youtube</a></li>
-    <li><a href="scheduler">Youtube scheduler tool</a></li>
-    <li><a href="sudoku">Sudoku tool</a></li>
-</ul>
+<main>
+	<h1>Synaptic Synergy</h1>
+	<ul>
+		<li><a href="https://www.youtube.com/@Synaptic-Synergy">Youtube channel</a></li>
+		<li><a href="scheduler">Youtube scheduler tool</a></li>
+		<li><a href="sudoku">Sudoku tool</a></li>
+	</ul>
+</main>
+
+<style>
+	main {
+		max-width: 300px;
+		margin: auto;
+	}
+</style>
