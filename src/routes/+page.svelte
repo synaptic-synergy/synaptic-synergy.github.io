15a2dba358a245a69c7f57c55d1354b6f9431a9d
Synaptic Synergy <synaptic.synergy.yt@gmail.com>
2024-04-16 16:28:09 +0200

    link github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	<h1>Synaptic Synergy</h1>
 	<ul>
 		<li><a href="https://www.youtube.com/@Synaptic-Synergy">Youtube channel</a></li>
+		<li><a href="https://github.com/synaptic-synergy">Github</a></li>
 		<li><a href="scheduler">Youtube scheduler tool</a></li>
 		<li><a href="sudoku">Sudoku tool</a></li>
 	</ul>
